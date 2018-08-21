b49f6df45a46088a8ff84cd26db094e72078877c
Howard Chong <hc757@cornell.edu>
2018-08-21 13:22:31 -0700

    starting animation, finished splash screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Asset, SplashScreen } from 'expo';
 import LoadingMeditation from './components/loadingMeditation'
 import Home from './components/home'
 import Review from './components/review'
@@ -9,6 +10,7 @@ export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      firstLoading: true,
       mode: 'loading' // loading, home, meditation, review
     }
   }
@@ -26,32 +28,55 @@ export default class App extends React.Component {
     this.setState({mode:'meditation'})
   }
 
+  componentDidMount() {
+    SplashScreen.preventAutoHide();
+  }
+
   render() {
+    if (this.state.firstLoading) {
+      return (
+        <View style={{ flex: 1}}>
+          <Image
+            source={require('./assets/SuzukiRoshi.png')}
+            onLoad={this._cacheResourcesAsync}
+          />
+        </View>
+      )
+    } else {
+      if (this.state.mode == 'loading'){
+        return (
+          <LoadingMeditation changeToHome={()=>this.changeToHome()}>
+          </LoadingMeditation>
+        );
+      } else if (this.state.mode == 'home'){
+        return (
+          <Home changeToMeditation={()=>this.changeToMeditation()}>
+          </Home>
+        )
+      } else if (this.state.mode == 'meditation'){
+        return (
+          <Meditation changeToReview={()=>this.changeToReview()}>
+          </Meditation>
+        )
+      } else if (this.state.mode == 'review'){
+        return(
+          <Review changeToHome={()=>this.changeToHome()}>
 
-    if(this.state.mode == 'loading'){
-      return (
-        <LoadingMeditation changeToHome={()=>this.changeToHome()}>
-        </LoadingMeditation>
-      );
-    } else if (this.state.mode == 'home'){
-      return (
-        <Home changeToMeditation={()=>this.changeToMeditation()}>
-        </Home>
-      )
-    } else if (this.state.mode == 'meditation'){
-      return (
-        <Meditation changeToReview={()=>this.changeToReview()}>
-        </Meditation>
-      )
-    } else if (this.state.mode == 'review'){
-      return(
-        <Review changeToHome={()=>this.changeToHome()}>
-
-        </Review>
-      )
+          </Review>
+        )
+      }
     }
   }
 
+  _cacheResourcesAsync = ()=> {
+    console.log("cache resources async, 3 second delay");
+    setTimeout(
+      ()=>{
+        SplashScreen.hide();
+        this.setState({firstLoading: false});
+      },
+      3000);
+  }
 }
 
 const styles = StyleSheet.create({
